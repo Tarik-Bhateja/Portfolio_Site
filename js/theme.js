7ac3ada7e3b19aa1e7984d131a92eb410428806e
Tarik Bhateja <77834838+Tarik-Bhateja@users.noreply.github.com>
2023-02-26 20:49:45 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -13,8 +13,8 @@
 // Preloader
 $(window).on('load', function () {
 	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
-	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(333);
+	$('.preloader').delay(151).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(151);
 });
 
 
